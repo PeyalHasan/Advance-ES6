f51c794576bdb049eed23739840fc315d33e78fb
unknown <hpeyal7@gmail.com>
2024-12-23 23:52:57 +0600

    first commit

---
+++ advance.js
@@ -0,0 +1,52 @@
+//advance es6 features explained
+// 1. Default Parameters
+// 2. Template Literals
+// 3. Multi-line Strings
+// 4. Destructuring Assignment
+// 5. Enhanced Object Literals
+// 6. Arrow Functions
+// 7. Promises
+// 8. Block-Scoped Constructs Let and Const
+// 9. Classes
+// 10. Modules
+// 11. Inheritance
+// 12. Generators
+// 13. Map, Set, WeakMap, WeakSet
+// 14. Typed Arrays
+// 15. New Built-In Methods
+// 16. Symbols
+// 17. Subclassable Built-ins
+// 18. Math + Number + String + Array + Object APIs
+// 19. Binary and Octal Literals
+// 20. Reflect API
+// 21. Tail Calls
+// 22. Exponentiation Operator
+// 23. Async Functions
+// 24. Trailing Commas
+// 25. Object.assign()
+// 26. Array.find()
+// 27. Array.findIndex()
+// 28. Array.from()
+// 29. Array.of()
+// 30. Array.fill()
+// 31. Array.entries()
+// 32. Array.keys()
+// 33. Array.values()
+// 34. Array.includes()
+// 35. TypedArray.from()
+// 36. TypedArray.of()
+// 37. TypedArray.copyWithin()
+// 38. TypedArray.entries()
+// 39. TypedArray.keys()
+// 40. TypedArray.values()
+// 41. Object.is()
+// 42. Number.isFinite()
+// 43. Number.isInteger()
+// 44. Number.isSafeInteger()
+// 45. Number.isNaN()
+// 46. Number.EPSILON
+// 47. Number.MIN_SAFE_INTEGER
+// 48. Number.MAX_SAFE_INTEGER
+// 49. Number.isSafeInteger()
+// 50. String.raw()
+// 51. String.fromCodePoint()
